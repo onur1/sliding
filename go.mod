ae587c673f097aca738bccad4417988e16c19f37
onur1 <onur1@onurgunduz.com>
2023-02-02 23:27:21 +0100

    Upgrade ring to v0.0.2

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/onur1/sliding
 
-go 1.17
+go 1.18
 
-require github.com/onur1/ring v0.0.0-20220101072901-3aa584118bff
+require github.com/onur1/ring v0.0.2
--- sliding.go
+++ sliding.go
@@ -8,41 +8,40 @@ import (
 )
 
 type Counter struct {
-	count chan int
+	count chan uint
+	peek  chan chan uint
 	exit  chan struct{}
-	peek  chan chan int
 }
 
 func (c *Counter) Close() {
 	close(c.exit)
 }
 
-func (c *Counter) Peek() int {
-	res := make(chan int, 1)
+func (c *Counter) Peek() uint {
+	res := make(chan uint, 1)
 	c.peek <- res
 	return <-res
 }
 
-func (c *Counter) Inc(n int) {
+func (c *Counter) Inc(n uint) {
 	c.count <- n
 }
 
-var (
-	log2        = math.Log(2)
-	minInterval = float64(time.Millisecond.Nanoseconds())
-)
-
-func loop(d time.Duration, cnt chan int, exit chan struct{}, peek chan chan int) {
+func loop(d time.Duration, cnt chan uint, exit chan struct{}, peek chan chan uint) {
 	var (
-		ptr  int = 0
-		head int = 0
+		ptr  uint = 0
+		head uint = 0
 	)
 
-	t := d.Nanoseconds()
-	l := int(math.Ceil(math.Log(float64(t)) / log2))
+	var a, b uint
 
-	list, ticker := ring.NewRing(l+1), time.NewTicker(
-		time.Duration(math.Max(float64(t/int64(l)), minInterval))*time.Nanosecond,
+	millis := uint(d.Milliseconds())
+	len := uint(math.Ceil(math.Log(float64(millis)) / math.Log(2)))
+
+	r := ring.NewRing[uint](len + 1)
+
+	ticker := time.NewTicker(
+		time.Duration(math.Max(float64((millis/len)), 250)) * time.Millisecond,
 	)
 
 LOOP:
@@ -50,17 +49,17 @@ LOOP:
 		select {
 		case i := <-cnt:
 			head = head + i
-			list.Put(ptr, head)
+			r.Put(ptr, head)
 		case <-ticker.C:
 			ptr = ptr + 1
-			ptr = list.Put(ptr, head)
+			ptr = r.Put(ptr, head)
 		case ch := <-peek:
-			a, b := 0, 0
-			if list.Get(ptr) != nil {
-				a = list.Get(ptr).(int)
+			a, b = 0, 0
+			if r.Get(ptr) != 0 {
+				a = r.Get(ptr)
 			}
-			if list.Get(ptr-l) != nil {
-				b = list.Get(ptr - l).(int)
+			if r.Get(ptr-len) != 0 {
+				b = r.Get(ptr - len)
 			}
 			ch <- a - b
 		case <-exit:
@@ -68,14 +67,18 @@ LOOP:
 		}
 	}
 
+	ticker.Stop()
+
+	r, ticker = nil, nil
+
 	close(cnt)
 	close(peek)
 }
 
 func NewCounter(d time.Duration) *Counter {
 	exit := make(chan struct{}, 1)
-	count := make(chan int)
-	peek := make(chan chan int)
+	count := make(chan uint)
+	peek := make(chan chan uint)
 
 	go loop(d, count, exit, peek)
 
