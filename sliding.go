0e0bc5bef94eab952cf3186e0003502823b6d52e
onur1 <onur1@onurgunduz.com>
2023-02-03 14:03:54 +0100

    Fixes and comments

--- sliding.go
+++ sliding.go
@@ -7,48 +7,72 @@ import (
 	"github.com/onur1/ring"
 )
 
+// A Counter is a sliding-window based counter which is used for
+// counting how many times something have occured within the last time frame.
 type Counter struct {
-	count chan uint
-	peek  chan chan uint
+	count chan struct{}
 	exit  chan struct{}
+	peek  chan chan int
 }
 
+// NewCounter returns a new Counter with the given frame duration.
+func NewCounter(d time.Duration) *Counter {
+	exit := make(chan struct{}, 1)
+	count := make(chan struct{})
+	peek := make(chan chan int)
+
+	go loop(d, count, exit, peek)
+
+	return &Counter{
+		count: count,
+		exit:  exit,
+		peek:  peek,
+	}
+}
+
+// Close stops the counter.
 func (c *Counter) Close() {
 	close(c.exit)
 }
 
-func (c *Counter) Peek() uint {
-	res := make(chan uint, 1)
+// Peek returns the current count.
+func (c *Counter) Peek() int {
+	res := make(chan int, 1)
 	c.peek <- res
 	return <-res
 }
 
-func (c *Counter) Inc(n uint) {
-	c.count <- n
+// Inc increments the counter by 1.
+func (c *Counter) Inc() {
+	c.count <- struct{}{}
 }
 
-func loop(d time.Duration, cnt chan uint, exit chan struct{}, peek chan chan uint) {
+var (
+	log2        = math.Log(2)
+	minInterval = float64(time.Millisecond.Nanoseconds())
+)
+
+func loop(d time.Duration, cnt chan struct{}, exit chan struct{}, peek chan chan int) {
 	var (
 		ptr  uint = 0
-		head uint = 0
+		head int  = 0
 	)
 
-	var a, b uint
+	t := d.Nanoseconds()
+	l := uint(math.Ceil(math.Log(float64(t)) / log2))
 
-	millis := uint(d.Milliseconds())
-	len := uint(math.Ceil(math.Log(float64(millis)) / math.Log(2)))
-
-	r := ring.NewRing[uint](len + 1)
-
+	r := ring.NewRing[int](l + 1)
 	ticker := time.NewTicker(
-		time.Duration(math.Max(float64((millis/len)), 250)) * time.Millisecond,
+		time.Duration(math.Max(float64(t/int64(l)), minInterval)) * time.Nanosecond,
 	)
+
+	var a, b int
 
 LOOP:
 	for {
 		select {
-		case i := <-cnt:
-			head = head + i
+		case <-cnt:
+			head = head + 1
 			r.Put(ptr, head)
 		case <-ticker.C:
 			ptr = ptr + 1
@@ -58,8 +82,8 @@ LOOP:
 			if r.Get(ptr) != 0 {
 				a = r.Get(ptr)
 			}
-			if r.Get(ptr-len) != 0 {
-				b = r.Get(ptr - len)
+			if r.Get(ptr-l) != 0 {
+				b = r.Get(ptr - l)
 			}
 			ch <- a - b
 		case <-exit:
@@ -69,22 +93,6 @@ LOOP:
 
 	ticker.Stop()
 
-	r, ticker = nil, nil
-
 	close(cnt)
 	close(peek)
-}
-
-func NewCounter(d time.Duration) *Counter {
-	exit := make(chan struct{}, 1)
-	count := make(chan uint)
-	peek := make(chan chan uint)
-
-	go loop(d, count, exit, peek)
-
-	return &Counter{
-		count: count,
-		exit:  exit,
-		peek:  peek,
-	}
 }
